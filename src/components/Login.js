ba25ce34054b4ff0e1a79352762a608af85b2355
Sovanroth13 <sovanroth016@gmail.com>
2023-10-31 11:07:48 +0700

    update

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,7 +7,7 @@ export default function Login() {
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <img
             className="mx-auto h-10 w-auto"
-            src="https://w7.pngwing.com/pngs/630/871/png-transparent-heart-eyes-emoji-emoji-heart-iphone-love-emoji-smiley-sticker-emoticon-thumbnail.png"
+            src="sovanroth.png"
           />
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
             Log in to your account
