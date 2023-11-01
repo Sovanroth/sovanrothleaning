53d392f946cc8204b555c4c7ad880fca393275e4
Sovanroth13 <sovanroth016@gmail.com>
2023-11-01 13:27:36 +0700

    setup redux

--- src/App.js
+++ src/App.js
@@ -11,20 +11,18 @@ import EditCoursePage from "./view/Teacher/EditCoursePage";
 
 function App() {
   return (
-    <div>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home/>}></Route>
-          <Route path="/browse" element={<Browse/>}></Route>
-          <Route path="/teacher-mode" element={<TeacherHome/>}></Route>
-          <Route path="/teacher-mode/create" element={<Create/>}></Route>
-          <Route path="/signup" element={<SignUpPage/>}></Route>
-          <Route path="/login" element={<LogInPage/>}></Route>
-          <Route path="/edit-course" element={<EditCoursePage/>}></Route>
-          <Route path="*" element={<NotFoundPage />}></Route>
-        </Routes>
-      </Router>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/browse" element={<Browse />}></Route>
+        <Route path="/teacher-mode" element={<TeacherHome />}></Route>
+        <Route path="/teacher-mode/create" element={<Create />}></Route>
+        <Route path="/signup" element={<SignUpPage />}></Route>
+        <Route path="/login" element={<LogInPage />}></Route>
+        <Route path="/edit-course" element={<EditCoursePage />}></Route>
+        <Route path="*" element={<NotFoundPage />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
