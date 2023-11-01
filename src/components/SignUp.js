770158bf3d55722abffad919b593edd1f3d72bbd
Sovanroth13 <sovanroth016@gmail.com>
2023-11-01 09:56:32 +0700

    update sign up

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -5,10 +5,7 @@ export default function SignUp() {
     <>
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img
-            className="mx-auto h-10 w-auto"
-            src="sovanroth.png"
-          />
+          <img className="mx-auto h-10 w-auto" src="sovanroth.png" />
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
             Create your Account
           </h2>
@@ -16,6 +13,25 @@ export default function SignUp() {
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
           <form className="space-y-6" action="#" method="POST">
+            <div>
+              <label
+                htmlFor="text"
+                className="block text-sm font-medium text-start leading-6 text-gray-900"
+              >
+                Username
+              </label>
+              <div className="mt-2">
+                <input
+                  id="username"
+                  name="username"
+                  type="text"
+                  // autoComplete="email"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
             <div>
               <label
                 htmlFor="email"
