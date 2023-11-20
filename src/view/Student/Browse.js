b8f6a51f372fc59fa757eedb7d7a5dd55b59b22d
Sovanroth13 <sovanroth016@gmail.com>
2023-11-20 14:42:38 +0700

    update

--- src/view/Student/Browse.js
+++ src/view/Student/Browse.js
@@ -2,14 +2,12 @@ import React from "react";
 import NavBar from "../../components/NavBar";
 import Footer from "../../components/Footer";
 import ExploreCourse from "../../components/ExploreCourse";
-import { isEmpty } from "@firebase/util";
-import { useState } from "react";
-import { useSelector } from "react-redux";
-import LoadingScreen from "../../components/LoadingScreen";
+// import { isEmpty } from "@firebase/util";
+// import { useState } from "react";
+// import { useSelector } from "react-redux";
+// import LoadingScreen from "../../components/LoadingScreen";
 
 const Browse = () => {
-  const [loading, setLoading] = useState(false);
-  const course = useSelector((state) => state?.courses?.data);
 
   return (
     <div>
