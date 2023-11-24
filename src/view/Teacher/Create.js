a3274162d26aa82b3919d8c15ef929bf412e8246
Sovanroth13 <sovanroth016@gmail.com>
2023-11-24 11:05:32 +0700

    update on create course

--- src/view/Teacher/Create.js
+++ src/view/Teacher/Create.js
@@ -2,17 +2,13 @@ import React from "react";
 import CreateCourse from "../../components/CreateCourse";
 import TeacherNavBar from "../../components/TeacherNavBar";
 import Footer from "../../components/Footer";
+import AddCourse from "../../components/AddCourse";
 
 const Create = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <TeacherNavBar />
-      <div className="flex-grow flex justify-center items-center">
-        <CreateCourse />
-      </div>
-      <footer>
-        <Footer />
-      </footer>
+      <AddCourse />
     </div>
   );
 };
