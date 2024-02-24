ee0e77b9fd2e0e91701371786238c7f2bca45666
Sovanroth13 <sovanroth016@gmail.com>
2024-02-24 19:11:24 +0700

    private route and role base

--- src/view/Auth/PrivateRoute.js
+++ src/view/Auth/PrivateRoute.js
@@ -1,13 +1,21 @@
 import React from "react";
-import { Route, Navigate } from "react-router-dom";
+import { Route, Navigate, Outlet } from "react-router-dom";
+
+// const PrivateRoute = ({ element, ...rest }) => {
+//   const token = localStorage.getItem("token");
+
+//   return token ? (
+//     <Route {...rest} element={element} />
+//   ) : (
+//     <Navigate to="/login" replace />
+//   );
+// };
 
 const PrivateRoute = ({ element, ...rest }) => {
   const token = localStorage.getItem("token");
 
-  return token ? (
-    <Route {...rest} element={element} />
-  ) : (
-    <Navigate to="/login" replace />
+  return(
+    token? <Outlet/> : <Navigate to="/login"/>
   );
 };
 
