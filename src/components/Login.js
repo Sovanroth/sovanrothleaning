20dd39c307aa8df6ce378bff73ed6b76c26ed190
Sovanroth13 <sovanroth016@gmail.com>
2024-03-15 19:18:24 +0700

    update login and signup ui

--- src/components/Login.js
+++ src/components/Login.js
@@ -71,14 +71,14 @@ export default function Login() {
       <div className="flex h-screen">
         <div className="flex-1 flex flex-col justify-center px-4 py-12 sm:px-6 lg:px-20 xl:px-24">
           <div className="mx-auto w-full max-w-sm lg:w-96">
-            <div>
+            <div class="flex flex-col items-center">
               <img
                 className="h-20 w-auto justify-center"
                 src="../sovanroth.png"
                 alt="Your Company"
               />
               <h2 className="mt-8 text-2xl font-bold leading-9 tracking-tight text-gray-900">
-                Sign in to your account
+                Log Into to your account
               </h2>
               <p className="mt-2 text-sm leading-6 text-gray-500">
                 Not a member?{" "}
@@ -199,7 +199,7 @@ export default function Login() {
         <div className="flex-1 hidden lg:block">
           <img
             className="h-full w-full object-cover"
-            src="https://images.unsplash.com/photo-1496917756835-20cb06e75b4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1908&q=80"
+            src="../4957136.jpg"
             alt=""
           />
         </div>
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -75,7 +75,146 @@ export default function SignUp() {
           infoMessage={errorMessage}
         />
       )}
-      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+
+      <div className="flex h-screen">
+        <div className="flex-1 flex flex-col justify-center px-4 py-12 sm:px-6 lg:px-20 xl:px-24">
+          <div className="mx-auto w-full max-w-sm lg:w-96">
+            <div class="flex flex-col items-center">
+              <img
+                className="h-20 w-auto justify-center"
+                src="../sovanroth.png"
+                alt="Your Company"
+              />
+              <h2 className="mt-8 text-2xl font-bold leading-9 tracking-tight text-gray-900">
+                Sign Up
+              </h2>
+              <p className="mt-2 text-sm leading-6 text-gray-500">
+                Already a member?{" "}
+                <Link
+                  to="../login"
+                  className="font-semibold text-indigo-600 hover:text-indigo-500"
+                >
+                  Sign in here
+                </Link>
+              </p>
+            </div>
+
+            <div className="mt-5">
+              <div>
+                <form
+                  className="space-y-6"
+                  method="POST"
+                  onSubmit={handleClickSignUp}
+                >
+                  <div>
+                    <label
+                      htmlFor="text"
+                      className="block text-sm font-medium text-start leading-6 text-gray-900"
+                    >
+                      Username
+                    </label>
+                    <div>
+                      <input
+                        value={data?.username}
+                        onChange={handleChangeUserName}
+                        type="text"
+                        required
+                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      />
+                    </div>
+                  </div>
+
+                  <div>
+                    <label
+                      htmlFor="email"
+                      className="block text-sm font-medium text-start leading-6 text-gray-900"
+                    >
+                      Email address
+                    </label>
+                    <div>
+                      <input
+                        value={data?.email}
+                        onChange={handleChangEmail}
+                        id="email"
+                        name="email"
+                        type="email"
+                        autoComplete="email"
+                        required
+                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      />
+                    </div>
+                  </div>
+
+                  <div>
+                    <div className="flex items-center justify-between">
+                      <label
+                        htmlFor="password"
+                        className="block text-sm font-medium leading-6 text-gray-900"
+                      >
+                        Password
+                      </label>
+                    </div>
+                    <div className="relative">
+                      <input
+                        value={data?.password}
+                        onChange={handleChangePassword}
+                        id="password"
+                        name="password"
+                        type={showPassword ? "text" : "password"}
+                        autoComplete="current-password"
+                        required
+                        className="block w-full rounded-md border-0 py-1.5 pr-10 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      />
+                      <button
+                        type="button"
+                        className="absolute inset-y-0 right-0 pr-3 flex items-center"
+                        onClick={togglePasswordVisibility}
+                      >
+                        {showPassword ? (
+                          <EyeOff className="h-5 w-5 text-gray-500" size={8} />
+                        ) : (
+                          <Eye className="h-5 w-5 text-gray-500" size={8} />
+                        )}
+                      </button>
+                    </div>
+                  </div>
+                  <div>
+                    <button
+                      type="submit"
+                      className="mt-4 flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    >
+                      {loading ? "Loading..." : "Sign up"}
+                    </button>
+                  </div>
+                </form>
+              </div>
+
+              <div className="mt-10">
+                <div className="relative">
+                  <div
+                    className="absolute inset-0 flex items-center"
+                    aria-hidden="true"
+                  >
+                    <div className="w-full border-t border-gray-200" />
+                  </div>
+                  <div className="relative flex justify-center text-sm font-medium leading-6"></div>
+                </div>
+
+                <div className="mt-6 grid grid-cols-2 gap-4"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex-1 hidden lg:block">
+          <img
+            className="h-full w-full object-cover"
+            src="../4957136.jpg"
+            alt=""
+          />
+        </div>
+      </div>
+
+      {/* <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <img className="mx-auto h-10 w-auto" src="sovanroth.png" alt="Logo" />
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
@@ -179,7 +318,7 @@ export default function SignUp() {
             </Link>
           </p>
         </div>
-      </div>
+      </div> */}
     </>
   );
 }
