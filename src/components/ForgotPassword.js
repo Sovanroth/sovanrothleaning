ad4cc16f53f11cd949a0b86da29b792fc26488d7
Sovanroth13 <sovanroth016@gmail.com>
2024-03-15 19:39:06 +0700

    update forget password ui

--- src/components/ForgotPassword.js
+++ src/components/ForgotPassword.js
@@ -1,29 +1,35 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const ForgotPassword = () => {
   return (
-    <div>
-      <div className=" mt-40 flex min-h-full flex-1 flex-col justify-center px-6 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img className="mx-auto h-10 w-auto" src="sovanroth.png" />
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Forgot Password
-          </h2>
-        </div>
+    <div className="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <img
+          className="mx-auto h-20 w-auto"
+          src="../sovanroth.png"
+          alt="Your Company"
+        />
+        <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          Find your account
+        </h2>
+      </div>
 
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form className="space-y-6" method="POST">
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
+        <div className="bg-white px-6 py-12 shadow sm:rounded-lg sm:px-12">
+          <div className="text-center text-sm text-gray-500">
+            Please enter your email to search for your account.
+          </div>
+          <form className="space-y-6 mt-5" action="#" method="POST">
             <div>
               <label
                 htmlFor="email"
-                className="block text-sm font-medium text-start leading-6 text-gray-900"
+                className="block text-sm font-medium leading-6 text-gray-900"
               >
                 Email address
               </label>
               <div className="mt-2">
                 <input
-                  // value={data?.email}
-                  // onChange={(e) => handleChangeEmail(e)}
                   id="email"
                   name="email"
                   type="email"
@@ -33,16 +39,27 @@ const ForgotPassword = () => {
                 />
               </div>
             </div>
+
             <div>
               <button
                 type="submit"
-                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 mt-8"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Request Reset Password
               </button>
             </div>
           </form>
         </div>
+
+        <p className="mt-5 text-center text-sm text-gray-500">
+          Already a member?{" "}
+          <Link
+            to="../login"
+            className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+          >
+            Sign in here
+          </Link>
+        </p>
       </div>
     </div>
   );
--- src/view/Auth/ForgotPasswordPage.js
+++ src/view/Auth/ForgotPasswordPage.js
@@ -3,7 +3,7 @@ import ForgotPassword from "../../components/ForgotPassword";
 
 const ForgotPasswordPage = () => {
   return (
-    <div className="flex flex-col min-h-screen bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-white to-gray-400">
+    <div className="flex flex-col min-h-screen bg-gray-50">
       <ForgotPassword />
       <div className="flex-grow"></div>
       <footer></footer>
