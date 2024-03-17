68f79c311cf3fa3bf47714a4ad6800acaffcc97f
Sovanroth13 <sovanroth016@gmail.com>
2024-03-17 12:13:06 +0700

    add more laoding time and get course by category

--- src/App.js
+++ src/App.js
@@ -26,27 +26,27 @@ import WatchVideos from "./view/Student/WatchVideos";
 import Example from "./test";
 
 function App() {
-  document.addEventListener("contextmenu", function (e) {
-    e.preventDefault();
-  });
+  // document.addEventListener("contextmenu", function (e) {
+  //   e.preventDefault();
+  // });
 
-  document.onkeydown = function (e) {
-    if (e.keyCode === 123) {
-      return false;
-    }
-    if (e.ctrlKey && e.shiftKey && e.keyCode == "I".charCodeAt(0)) {
-      return false;
-    }
-    if (e.ctrlKey && e.shiftKey && e.keyCode == "C".charCodeAt(0)) {
-      return false;
-    }
-    if (e.ctrlKey && e.shiftKey && e.keyCode == "J".charCodeAt(0)) {
-      return false;
-    }
-    if (e.ctrlKey && e.shiftKey && e.keyCode == "U".charCodeAt(0)) {
-      return false;
-    }
-  };
+  // document.onkeydown = function (e) {
+  //   if (e.keyCode === 123) {
+  //     return false;
+  //   }
+  //   if (e.ctrlKey && e.shiftKey && e.keyCode == "I".charCodeAt(0)) {
+  //     return false;
+  //   }
+  //   if (e.ctrlKey && e.shiftKey && e.keyCode == "C".charCodeAt(0)) {
+  //     return false;
+  //   }
+  //   if (e.ctrlKey && e.shiftKey && e.keyCode == "J".charCodeAt(0)) {
+  //     return false;
+  //   }
+  //   if (e.ctrlKey && e.shiftKey && e.keyCode == "U".charCodeAt(0)) {
+  //     return false;
+  //   }
+  // };
 
   return (
     <Router>
