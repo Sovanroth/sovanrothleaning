dd58b543bc75eb13cab3ae510758526ddcc4aa62
Sovanroth13 <sovanroth016@gmail.com>
2024-03-31 14:34:02 +0700

    update exp token

--- src/view/Student/Home.js
+++ src/view/Student/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import NavBar from "../../components/NavBar";
 import Footer from "../../components/Footer";
 import CourseOwned from "../../components/CourseOwned";
@@ -7,7 +7,8 @@ import { Helmet } from "react-helmet";
 const Home = () => {
   // console.log(localStorage.getItem("userId"));
   // console.log(localStorage.getItem("token"));
-  // console.log(localStorage.getItem("role"));
+  // console.log(localStorage.getItem("role"))
+  
   return (
     <div className="flex flex-col min-h-screen">
       <Helmet>
