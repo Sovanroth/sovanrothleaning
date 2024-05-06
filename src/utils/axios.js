da7366f64fc616daf0d2387b615c9c6f05d976b9
Sovanroth13 <sovanroth016@gmail.com>
2024-05-06 23:32:01 +0700

    update endpoint

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // const axiosInstance = axios.create({ baseURL: "http://localhost:3000" });
 const axiosInstance = axios.create({
-  baseURL: "https://sovanrothlearningserver-production.up.railway.app/",
+  baseURL: "https://lms.sovanrothnath.site/",
 });
 
 axiosInstance.interceptors.response.use(
