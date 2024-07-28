4d699450db9217caeadfeb64f135c8b6d9ccac89
Sovanroth13 <sovanroth016@gmail.com>
2024-07-28 19:06:20 +0700

    udpate end-point

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // const axiosInstance = axios.create({ baseURL: "http://localhost:3000" });
 const axiosInstance = axios.create({
-  baseURL: "https://server.sovanrothnath.site/",
+  baseURL: "https://suku-server.sovanrothnath.site/",
 });
 
 axiosInstance.interceptors.response.use(
